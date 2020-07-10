63b00335095a6ee44506616887207d97278bbc4c
Dung Dao <dung.dao19@outlook.com>
2020-07-09 22:24:29 -0500

    Updates

---
+++ precache-manifest.c2d2cec4c7365c41da6567d1b3bea15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c799bc6778a6d2a5521bd742fe369b8d",
+    "url": "/movie_database/index.html"
+  },
+  {
+    "revision": "ec6f9ea47b99c8b319d3",
+    "url": "/movie_database/static/css/main.27f5e03a.chunk.css"
+  },
+  {
+    "revision": "43b5adafc939ea282e80",
+    "url": "/movie_database/static/js/2.591974ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_database/static/js/2.591974ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6f9ea47b99c8b319d3",
+    "url": "/movie_database/static/js/main.f3d02922.chunk.js"
+  },
+  {
+    "revision": "7677c51a727165442fee",
+    "url": "/movie_database/static/js/runtime-main.44198587.js"
+  }
+]);
